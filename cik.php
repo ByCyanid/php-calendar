816b8cc007d4d0deaee30db8e32b708439937ea5
ByCyanid <62667140+ByCyanid@users.noreply.github.com>
2021-07-12 13:56:53 +0300

    Update cik.php

--- cik.php
+++ cik.php
@@ -1,7 +1,12 @@
 <?php 
-		session_start();
-	    session_destroy();
-		header("Location: login.php");	
-			
 
+	session_start();
+	if(!$_SESSION['id']){
+ 	header("location:login.php");// sayfa.php eğer session yok ise yönlendirilecek sayfadır
+	}
+	else{
+		session_destroy();
+		header("Location: login.php");	
+	}
+	
 ?>
